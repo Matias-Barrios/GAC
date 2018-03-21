7b7aefeb3c439d1b6b830b5cbf347b8a4b6f5536
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-03-21 14:27:52 -0500

    Update gac.cpp

--- gac.cpp
+++ gac.cpp
@@ -13,7 +13,6 @@ using namespace std;
 
 
 void Error(int code) {
-    
     if ( code == 2 ) {
          cout << "Error! This application only accepts a single argument!!" << endl;
          exit(code);
@@ -27,19 +26,19 @@ void Error(int code) {
 bool lookupTerm(const std::string& term, const std::vector<std::string>& possible_names) {
     for (const std::string &possible_name : possible_names)
     {
-        if (possible_name.compare(term) == 0)
+        cout << "possible_name  : " << possible_name << endl;
+        cout << "term  : " << term << endl;
+        if (possible_name == term)
             return true;
     }
     return false;
 }
 void searchTerm(string term) {
-    bool values_found = false;
     if ( lookupTerm(term,possible_tilde) ) {
         cout << possible_tilde_message << endl;
-        values_found = true;
+        Error(0);
     }
-    if ( values_found == false )
-        Error(3);
+    Error(3);
 }
 
 int main(int argc, char *argv[]) {
