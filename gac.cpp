2d5297a5a4e0bd23aef5725e7d2419e95c8d36cb
Matias.Barrios <30472800+Matias-Barrios@users.noreply.github.com>
2018-04-02 12:11:51 -0500

    Update gac.cpp

--- gac.cpp
+++ gac.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>   
+
 
 using namespace std;
 
 // GLOBAL constants for each character
 
+
+//  Character : !
+    const std::vector<std::string> possible_exclmark = { "exclamation", "exclamation symbol" , "exclamation mark"};
+    const string possible_exclmark_message = "! | 33";
+//  ***********************
+//  Character : "
+    const std::vector<std::string> possible_exclmark = { "double quotation", "double quotation marks" , "double quotes"};
+    const string possible_exclmark_message = "\" | 34";
+//  ***********************
+//  Character : #
+    const std::vector<std::string> possible_hash = { "number sign", "hash" , "pound" , "double quotes"};
+    const string possible_exclmark_message = "# | 35";
+//  ***********************
+//  Character : $
+    const std::vector<std::string> possible_dollar = { "dollar", "dollar symbol", "dollar sign" };
+    const string possible_exclmark_message = "$ | 36";
+//  ***********************
+//  Character : %
+    const std::vector<std::string> possible_percent = { "percent", "percent symbol", "percent sign" , "percentage" };
+    const string possible_exclmark_message = "% | 37";
+//  ***********************
+//  Character : &
+    const std::vector<std::string> possible_ampersand = { "percent", "percent symbol", "percent sign" , "percentage" };
+    const string possible_exclmark_message = "& | 38";
+//  ***********************
+//  Character : '
+    const std::vector<std::string> possible_exclmark = { "apostrophe", "single quote", "single quotation"  };
+    const string possible_exclmark_message = "' | 39";
+//  ***********************
 //  Character : ~
     const std::vector<std::string> possible_tilde = { "tilde", "swung dash" , "ñoqui"};
     const string possible_tilde_message = "~ | 126";
@@ -24,21 +55,22 @@ void Error(int code) {
 }
 
 bool lookupTerm(const std::string& term, const std::vector<std::string>& possible_names) {
+    std::string lower(term);
+    transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
     for (const std::string &possible_name : possible_names)
     {
-        cout << "possible_name  : " << possible_name << endl;
-        cout << "term  : " << term << endl;
-        if (possible_name == term)
+        if (possible_name.compare(lower) == 0)
             return true;
     }
     return false;
 }
 void searchTerm(string term) {
+    bool one_found = false;
     if ( lookupTerm(term,possible_tilde) ) {
         cout << possible_tilde_message << endl;
-        Error(0);
+        one_found = true;
     }
-    Error(3);
+    ( ! one_found ) ? Error(3) : Error(0);
 }
 
 int main(int argc, char *argv[]) {
